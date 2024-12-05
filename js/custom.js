9054a64252d703ad11e8abcb24ac097c9d240920
theonlyshannon <sofwannuhaalfaruq@gmail.com>
2024-12-05 09:14:30 +0700

    js update

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,3 @@
-$(document).ready(function () {
-    $('#example').DataTable({
-        pageLength: 10, // Menampilkan 10 baris per halaman
-        lengthMenu: [5, 10, 25, 50, 100], // Pilihan jumlah data yang dapat dilihat
-        responsive: true, // Membuat tabel responsif
-        language: {
-            search: "Cari:",
-            lengthMenu: "Tampilkan _MENU_ data",
-            info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
-            paginate: {
-                first: "Awal",
-                last: "Akhir",
-                next: "Berikutnya",
-                previous: "Sebelumnya"
-            }
-        }
-    });
-});
-
-
 document.querySelectorAll('.link-label').forEach(function(link) {
     link.addEventListener('click', function(event) {
         event.preventDefault(); 
